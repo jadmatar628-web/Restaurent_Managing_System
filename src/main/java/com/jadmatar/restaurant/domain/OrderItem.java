23738e5f2e3fed4cdfb07174e60786d44de1ecac
Jad Matar <jadmatar628@gmail.com>
2026-08-13 19:48:55 +0300

    OrderItem class is done version 1

--- src/main/java/com/jadmatar/restaurant/domain/OrderItem.java
+++ src/main/java/com/jadmatar/restaurant/domain/OrderItem.java
@@ -3,24 +3,38 @@ package com.jadmatar.restaurant.domain;
 import java.math.BigDecimal;
 
 public class OrderItem {
-
     private final MenuItem menuItem;
-    private int quantity;
     private final BigDecimal unitPrice;
-    private BigDecimal discount;
+    private int quantity;
+    private final BigDecimal discountPercentage;
 
-    public OrderItem(MenuItem menuItem, int quantity,BigDecimal unitPrice,BigDecimal discount) {
-        if(menuItem==null){
+    public OrderItem(MenuItem menuItem, int quantity, BigDecimal unitPrice, BigDecimal discountPercentage) {
+        if (menuItem == null) {
             throw new IllegalArgumentException("Menu Item cannot be null");
         }
         this.menuItem = menuItem;
-        if(quantity==0){
+        if (quantity <= 0) {
             throw new IllegalArgumentException("Quantity cannot be 0");
         }
         this.quantity = quantity;
-        if(unitPrice.compareTo(BigDecimal.ZERO)<0){
-            throw new IllegalArgumentException("Price cannot be 0");
+        if (unitPrice == null) {
+            throw new IllegalArgumentException("Unit price cannot be null");
+        }
+        if (unitPrice.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("Price cannot be less than 0");
         }
         this.unitPrice = unitPrice;
+        if (discountPercentage == null) {
+            discountPercentage = BigDecimal.ZERO;
+        }
+
+        if (discountPercentage.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("Discount cannot be negative");
+        }
+
+        if (discountPercentage.compareTo(BigDecimal.valueOf(100)) > 0) {
+            throw new IllegalArgumentException("Discount cannot exceed 100%");
+        }
+        this.discountPercentage=discountPercentage;
     }
 }
