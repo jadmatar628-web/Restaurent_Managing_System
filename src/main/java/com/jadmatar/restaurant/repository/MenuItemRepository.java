5cac4c640c8c893ed5fb1db6d1cdd2651942b71b
Jad Matar <jadmatar628@gmail.com>
2026-08-20 21:09:21 +0300

    nothing

--- src/main/java/com/jadmatar/restaurant/repository/MenuItemRepository.java
+++ src/main/java/com/jadmatar/restaurant/repository/MenuItemRepository.java
@@ -1,33 +1,14 @@
 package com.jadmatar.restaurant.repository;
-
 import com.jadmatar.restaurant.domain.MenuItem;
 
-import java.util.ArrayList;
+import java.util.List;
 
-public class MenuItemRepository {
-    private final ArrayList<MenuItem> menuItems = new ArrayList<>();
+public interface MenuItemRepository {
+    void add(MenuItem menuItem);
 
-    public void addItem(MenuItem item) {
-        if (item == null) {
-            throw new IllegalArgumentException("ITEM CANNOT BE NULL");
-        }
-        for (MenuItem old : menuItems) {
-            if (old.getId() == item.getId()) {
-                throw new IllegalArgumentException("ITEM ALREADY EXISTS");
-            }
-        }
-        menuItems.add(item);
-    }
-    public MenuItem findById(int id){
-        for (MenuItem menuItem : menuItems) {
-            if (menuItem.getId() == id) {
-                return menuItem;
-            }
-        }
-        return null;
-    }
-    public ArrayList<MenuItem> findAll()
-    {
-        return new ArrayList<>(menuItems);
-    }
+    MenuItem findById(int id);
+
+    List<MenuItem> findAll();
+
+    void update(MenuItem menuItem);
 }
--- src/main/java/com/jadmatar/restaurant/repository/OrderRepository.java
+++ src/main/java/com/jadmatar/restaurant/repository/OrderRepository.java
@@ -4,7 +4,7 @@ import com.jadmatar.restaurant.domain.Order;
 import java.util.List;
 
 public interface OrderRepository {
-    Order order(Order order);
+    Order add(Order order);
     Order findById(int id);
     List<Order> findAll();
     void update(Order order);
